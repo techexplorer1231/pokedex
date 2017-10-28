a8aed957cb5131aebf14d03e7221341ced297d2c
Pratap <dreamweaver1231@gmail.com>
2017-10-28 23:50:36 +0530

    added initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent
   ],
   imports: [
     NgbModule.forRoot(),
